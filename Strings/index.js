5c76b189520f5849929034c647eea6a2603d6f2d
alihassan_ASE <ali.hassan@focusteck.com>
2023-10-12 13:24:39 +0500

    Added

--- Strings/index.js
+++ Strings/index.js
@@ -1,35 +1,26 @@
-// Ways to write string
-//With single Quote (It is single line string)
-let singleQuote = 'Using Single Quotes';
-console.log(singleQuote);
+//  If else conditons syntax
 
-// with double quotes (It is single line string also)
-let doubleQuotes = "Using Double Quotes";
-console.log(doubleQuotes);
+let num = 20;
 
-// with backticks (Also know as template string and is used for multi-line string)
-let backTicks = `This is 
-multi-line
-string`;
-console.log(backTicks);
+if(num === 20){
+  console.log('Hello i am 20 years old');
+}
+else if (num === 30){
+  console.log('hello i am 30 years old');
+}
+else {
+  console.log('Invalid');
+}
 
-// String Interpolation (Insertion of variable in string)
-const name = "Ali Hassan Mughal";
-let age = 22;
-let detail = `My name is ${name}, and my age is ${age}`;
+// Switch Statement
 
-console.log(detail);
-
-
-// You can insert a quotation mark inside a string by preceding it with a backslash.
-
-let quoteInString = "I am reading \"Example Book\" written by someone.";
-console.log(quoteInString);
-
-// Broken in Multiple lines but output will be in single line
-const str =
-  "this string \
-is broken \
-across multiple \
-lines.";
-console.log(str);
+switch(num){
+  case 1:
+    console.log('Number is 1');
+    break;
+  case 10:
+    console.log('Number is 10');
+    break;
+  default:
+    console.log('By default number is 40');
+}
