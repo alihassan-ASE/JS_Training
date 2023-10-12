ece3605e4ba9af5bd45ce5c0de2c1d8c72646b18
alihassan_ASE <ali.hassan@focusteck.com>
2023-10-12 16:38:08 +0500

    Added

--- Loops/index.js
+++ Loops/index.js
@@ -6,3 +6,18 @@ for (let stepsCount = 0; stepsCount < 10; stepsCount++){
 }
 
 
+// Example 2 find missing numbers
+
+let arr = [1, 2, 3, 5, 8, 10];
+let missingNumbers = [];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  while (arr[i] + 1 !== arr[i + 1]) {
+    arr[i]++;
+    missingNumbers.push(arr[i]);
+  }
+}
+
+console.log(missingNumbers);
+
+// Example 
