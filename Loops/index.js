42edb433f7b66a6083c024f0b4c7fd4acb02e9cd
alihassan_ASE <ali.hassan@focusteck.com>
2023-10-12 18:18:25 +0500

    Added

--- Loops/index.js
+++ Loops/index.js
@@ -18,6 +18,35 @@ for (let i = 0; i < arr.length - 1; i++) {
   }
 }
 
-console.log(missingNumbers);
+console.log('Missing Numbers are '+ missingNumbers);
 
-// Example 
+// Example 3 (Find Factorial)
+let number = 4;
+for (let i=number-1; i>1; i--){
+    number = number*i;
+}
+console.log('Factorial of given Number is ' + number);
+
+
+
+
+// While Loop 
+// Example 1(Find Length of String)
+let str = 'Ali Hassan';
+let length = 0;
+while(str[length]){
+    length++;
+}
+console.log('The length of Given string is '+ length);
+
+
+// Example 2 (Find Sum of number to n)
+let results = 0;
+let n = 1;
+while (n<=5){
+    results += n;
+    n++;
+}
+console.log(results);
+
+// Example 3 (Palindrome Check)
