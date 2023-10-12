68cf808df0b2da7f88fd38d9386f28a2982dd382
alihassan_ASE <ali.hassan@focusteck.com>
2023-10-12 15:04:43 +0500

    Added Example

---
+++ Conditions/index.js
@@ -0,0 +1,41 @@
+//  If else conditons syntax
+
+// Example 1
+let num = 20;
+
+if(num === 20){
+  console.log('Hello i am 20 years old');
+}
+else if (num === 30){
+  console.log('hello i am 30 years old');
+}
+else {
+  console.log('Invalid');
+}
+
+// Example 2
+let cheese = 'Chedder';
+
+if (cheese) {
+  console.log("Yay! Cheese available for making cheese on toast.");
+} else {
+  console.log("No cheese on toast for you today.");
+}
+
+
+
+
+// Switch Statement
+let number = 19;
+switch(number){
+  case 1:
+    console.log('Number is 1');
+    break;
+  case 10:
+    console.log('Number is 10');
+    break;
+  default:
+    console.log('By default number is 40');
+}
+
+
---
+++ Loops/index.js
@@ -0,0 +1,3 @@
+for (let steps = 0; steps < 5; steps++){
+    console.log(`Walking Step by Step and count step ${steps}`);
+}
